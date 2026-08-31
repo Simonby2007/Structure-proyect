8319f30cc72dc291c6c6d041ed93d77489db7da0
Simonby2007 <simon.usmaja@amigo.edu.co>
2026-08-30 22:31:32 -0500

    Condicional mayor, menor, cambiar monto

--- Proyecto Estructura de datos/main.java
+++ Proyecto Estructura de datos/main.java
@@ -4,9 +4,9 @@ public class main {
     public static void main(String[] args)
     {
     Scanner scanner = new Scanner (System.in);
-    String salir = "";
-    
-    while (!salir.equals("exit")) 
+    boolean salir = false;
+    String ingreso = "";
+    while (!salir) 
         //INGRESO DEL USUARIO
         {
         System.out.println("Ingrese nombre:");
@@ -25,42 +25,126 @@ public class main {
 
         System.out.println("Bienvenido " + nombre + " al simulador de inversiones");
         System.out.println("Monto actual -> " + monto + "$");
-        while (!salir.equals("enter") && !salir.equals("exit")) 
+        while (!salir && !ingreso.equals("enter")) 
         {
         System.out.println("");
         System.out.println("Ingresar: enter");
         System.out.println("Salir: exit");
-        salir = scanner.next();
+        ingreso = scanner.next();
+        if (ingreso.equals("exit")) 
+            {
+                salir = true;
+                System.out.println("Gracias por usar el simulador de inversiones");
+            }
+            else if (ingreso.equals("enter")) 
+            {
+
+            }else 
+            {
+                System.out.println("Error: Ingrese un valor valido");
+            }
         }
-        while (!salir.equals("exit")) 
+        while (!salir) 
             {
             //CREACIÓN DEL VECTOR GENERADOR DE NUMEROS ALEATORIOS
-            int [] numerosApuesta = new int[100];
-            int a = 0;
-            for (int i = 1; i <= 100; i++)
+                int [] numerosApuesta = new int[100];
+                int a = 0;
+                for (int i = 1; i <= 100; i++)
                 {   
-                numerosApuesta[a] = i;
-                a++;
+                    numerosApuesta[a] = i;
+                    a++;
                 }
-            Random aleatorio = new Random();
-            int indice = aleatorio.nextInt(numerosApuesta.length);
-            int valorElegido = numerosApuesta[indice];
-            System.out.println("PRIMER VALOR: " + valorElegido);
-            System.out.println("¿Cuánto desea ingresar?");
-            int montoTemp = scanner.nextInt();
-            while (montoTemp > monto) 
+                Random aleatorio = new Random();
+                int indice = aleatorio.nextInt(numerosApuesta.length);
+                int ValorElegido = numerosApuesta[indice];
+                System.out.println("PRIMER VALOR: " + ValorElegido);
+                System.out.println("¿Cuánto desea ingresar?");
+                int montoTemp = scanner.nextInt();
+                while (montoTemp > monto) 
+                    {
+                    System.out.println("Error: Ingrese un valor menor o igual al monto total");
+                    montoTemp = scanner.nextInt();
+                    }
+                monto = monto - montoTemp;
+                //EMPEZAR ALGORITMO DE COMPARACIÓN
+                while (!salir)
                 {
-                System.out.println("Error: Ingrese un valor menor o igual al monto total");
-                montoTemp = scanner.nextInt();
+                    int indice2 = aleatorio.nextInt(numerosApuesta.length);
+                    int ValorElegido2 = numerosApuesta[indice2];
+                    System.out.println("¿El siguiente numero será:");
+                    System.out.println("MAYOR: 1.)");
+                    System.out.println("MENOR: 2.)");
+                    System.out.println("Ingresar nuevo valor para apostar: 3.)");
+                    System.out.println("salir: exit");
+                    String comparacion = scanner.next();
+                    switch (comparacion) 
+                    {
+                        case "1":
+                            indice2 = aleatorio.nextInt(numerosApuesta.length);
+                            ValorElegido2 = numerosApuesta[indice2];
+                            System.out.println("SEGUNDO VALOR: " + ValorElegido2);
+                            if (ValorElegido2 > ValorElegido)
+                            {
+                                monto = monto + montoTemp;
+                                System.out.println("Ganaste! Monto actual -> " + monto + "$");
+                                ValorElegido = ValorElegido2;
+                            }
+                            else
+                            {
+                                System.out.println("Perdiste! Monto actual -> " + monto + "$");
+                                if (monto == 0)
+                                {
+                                    System.out.println("No tienes dinero para seguir jugando");
+                                    salir = true;
+                                }
+                            }
+                            ValorElegido = ValorElegido2;
+                            break;
+                        case "2":
+                            indice2 = aleatorio.nextInt(numerosApuesta.length);
+                            ValorElegido2 = numerosApuesta[indice2];
+                            System.out.println("SEGUNDO VALOR: " + ValorElegido2);
+                            if (ValorElegido2 < ValorElegido)
+                            {
+                                montoTemp = montoTemp * 2;
+                                monto = monto + montoTemp;
+                                System.out.println("Ganaste! Monto actual -> " + monto + "$");
+                                ValorElegido = ValorElegido2;
+                            }else
+                            {
+                                System.out.println("Perdiste! Monto actual -> " + monto + "$");
+                                if (monto == 0)
+                                {
+                                    System.out.println("No tienes dinero para seguir jugando");
+                                    salir = true;
+                                }
+                            }
+                            ValorElegido = ValorElegido2;
+                            break;
+                        case "3":
+                            monto = monto + montoTemp;
+                            System.out.println("Ingrese nuevo valor para apostar:");
+                            montoTemp = scanner.nextInt();
+                            while (montoTemp > monto) 
+                                {
+                                System.out.println("Error: Ingrese un valor menor o igual al monto total");
+                                montoTemp = scanner.nextInt();
+                                }
+                            monto = monto - montoTemp;
+                            break;
+                        case "exit":
+                            salir = true;
+                            System.out.println("Gracias por usar el simulador de inversiones");
+                            break;
+                        default:
+                            System.out.println("Error: Ingrese un valor valido");
+                            break;
+                    }
                 }
-            monto = monto - montoTemp;
-            System.out.println("¿El siguiente numero será mayor o menor?");
-            System.out.println("Salir: exit");
-            salir = scanner.next();
+            
             }
-        
         }
-
+        scanner.close();
     }
-    
 }
+
